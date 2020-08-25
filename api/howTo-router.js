7838d6d59c9d51be066671a1ea150d8922957495
Jason Hinton <jason_hinton@att.net>
2020-08-24 21:00:18 -0400

    resolving deploy issues

--- api/howTo-router.js
+++ api/howTo-router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const users = require("../dbHelpers/user-model.js");
 const restricted = require("../auth/restricted.js");
 
-router.get("/", (req, res) => {
+router.get("/", restricted, (req, res) => {
 	users
 		.listUsers()
 		.then((users) => {
