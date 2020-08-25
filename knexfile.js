2b5049333a07c88389f32b8007e4f47ceb1b7678
Jason Hinton <jason_hinton@att.net>
2020-08-24 22:40:53 -0400

    production

--- knexfile.js
+++ knexfile.js
@@ -23,11 +23,15 @@ module.exports = {
 	},
 
 	production: {
-		client: "pg",
-		connection: pgConnection,
+		client: "sqlite3",
+		useNullAsDefault: true,
+		connection: {
+			filename: "./data/users.db3",
+		},
 		pool: {
-			min: 2,
-			max: 10,
+			afterCreate: (conn, done) => {
+				conn.run("PRAGMA foreign_keys = ON", done);
+			},
 		},
 		migrations: {
 			directory: "./data/migrations",
