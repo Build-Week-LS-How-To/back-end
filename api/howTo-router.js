09786e0c3653ef186bee2e449c55bddeacce9ed8
Jason Hinton <jason_hinton@att.net>
2020-08-27 22:11:48 -0400

    get users endpoint

--- api/howTo-router.js
+++ api/howTo-router.js
@@ -10,12 +10,24 @@ router.get("/", restricted, (req, res) => {
 	users
 		.listUsers()
 		.leftJoin("hacks", "users.id", "hacks.userID")
-		// .select("users.firstName", "users.lastName", "hacks.*")
+		.select("users.firstName", "users.lastName", "hacks.*")
 		.then((hacks) => {
 			res.status(200).json(hacks);
 		})
 		.catch((err) => {
 			res.status(500).json({ message: "Error fetching hacks", err });
+		});
+});
+
+//Gets users
+router.get("/users", restricted, (req, res) => {
+	users
+		.listUsers()
+		.then((users) => {
+			res.status(200).json(users);
+		})
+		.catch((err) => {
+			res.status(500).json(err);
 		});
 });
 
